93227a44826c64c8fad1b2e6a2ec144c0c405388
David Clark <dmclark@gmail.com>
2011-06-05 10:08:56 -0400

    schema update

--- db/schema.rb
+++ db/schema.rb
@@ -9,26 +9,131 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100123231218) do
+ActiveRecord::Schema.define(:version => 20101105232475) do
 
-  create_table "users", :force => true do |t|
-    t.string   "login",                     :limit => 40
-    t.string   "name",                      :limit => 100, :default => ""
-    t.string   "first_name"
-    t.string   "last_name"
-    t.string   "email",                     :limit => 100
-    t.string   "crypted_password",          :limit => 40
-    t.string   "salt",                      :limit => 40
+  create_table "categories", :force => true do |t|
+    t.string   "name"
+    t.string   "meta"
+    t.integer  "parent_id"
+    t.integer  "taxonomy_id"
+    t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "remember_token",            :limit => 40
-    t.datetime "remember_token_expires_at"
-    t.string   "activation_code",           :limit => 40
-    t.datetime "activated_at"
-    t.string   "state",                                    :default => "passive"
-    t.datetime "deleted_at"
-    t.integer  "validation_id"
-    t.text     "roles"
+    t.integer  "translation_id"
+  end
+
+  create_table "category_collections", :force => true do |t|
+    t.integer  "category_id"
+    t.integer  "collection_id"
+    t.string   "status"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "collection_users", :force => true do |t|
+    t.integer  "collection_id"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "collections", :force => true do |t|
+    t.string   "name"
+    t.string   "status"
+    t.string   "organization"
+    t.string   "privacy"
+    t.string   "meta"
+    t.string   "url_about"
+    t.text     "contact_info"
+    t.text     "notes"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.text     "country"
+    t.text     "state"
+    t.text     "city"
+  end
+
+  create_table "languages", :force => true do |t|
+    t.string   "bibliographic_code", :limit => 3, :null => false
+    t.string   "terminologic_code"
+    t.string   "digit2_code"
+    t.integer  "sort_group"
+    t.string   "english_name",                    :null => false
+    t.string   "french_name",                     :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "posts", :force => true do |t|
+    t.string   "post_title"
+    t.text     "post_text"
+    t.string   "post_author"
+    t.string   "author_email"
+    t.integer  "topic_id"
+    t.integer  "collection_id"
+    t.integer  "category_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "taxonomy_id"
+  end
+
+  create_table "relationships", :force => true do |t|
+    t.integer  "category_id"
+    t.integer  "synonym_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "taxonomies", :force => true do |t|
+    t.string   "name"
+    t.string   "status"
+    t.string   "privacy"
+    t.string   "meta"
+    t.string   "url_about"
+    t.string   "url_category_html"
+    t.string   "url_category_xml"
+    t.integer  "language_id"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "internationalization_id"
+  end
+
+  create_table "taxonomy_users", :force => true do |t|
+    t.integer  "taxonomy_id"
+    t.integer  "user_id"
+    t.integer  "language_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "team_users", :force => true do |t|
+    t.integer  "team_id"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "teams", :force => true do |t|
+    t.string   "name"
+    t.text     "description"
+    t.string   "privacy"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "topics", :force => true do |t|
+    t.string   "topic_name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "validations", :force => true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
