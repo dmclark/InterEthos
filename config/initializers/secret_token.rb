5c9d08150a4abb5f3b1ee7d1cfd785185c891b80
David Clark <dmclark@gmail.com>
2011-06-05 21:51:57 -0400

    partal upgrade to 3

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Interethos::Application.config.secret_token = '888a6525a99f391c88f61e202b7e12d6b63f0849894168ab1fdfee17ca8f1c9267b9d0b7d5dd97d440b247153f0f626329f1e2a3050103839d68e4f5595711dc'
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,15 +1,8 @@
 # Be sure to restart your server when you modify this file.
 
-# Your secret key for verifying cookie session data integrity.
-# If you change this key, all old sessions will become invalid!
-# Make sure the secret is at least 30 characters and all random, 
-# no regular words or you'll be exposed to dictionary attacks.
-ActionController::Base.session = {
-  :key         => '_interethos_session',
-  :secret      => '5c822c0f57db8c4fd4e28a18be777521ee4e705275fc0f4d670e92f9a4625fb37a47aedf5bdbd7357723714a9b2421396cc9379cb1ad2832090da9d785b1738d'
-}
+Interethos::Application.config.session_store :cookie_store, :key => '_interethos_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
-# (create the session table with "rake db:sessions:create")
-# ActionController::Base.session_store = :active_record_store
+# (create the session table with "rails generate session_migration")
+# Interethos::Application.config.session_store :active_record_store
